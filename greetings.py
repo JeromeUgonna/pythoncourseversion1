c8d5faaa49eb649080621c2b832f32f4b5bead39
JeromeUgonna <ejikejerome@gmail.com>
2024-07-22 17:00:57 +0100

    starting a python course

---
+++ greetings.py
@@ -0,0 +1,6 @@
+name = input("what is your name \n")
+course_enrolled = input ("what is the programming course you enrolled for? \n")
+duration_of_course = input("Enter the course duration \n")
+company = input ("Comapny you registered with \n")
+
+print ("Hello " + name + "welcome to " + company + ". You enrolled for " + course_enrolled + " which will last for duration of " + duration_of_course )
